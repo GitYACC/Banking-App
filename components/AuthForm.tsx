7239e77ae22398cce930761b6c618f34a1c312cc
GitYACC <shastrysamarth@gmail.com>
2024-11-16 21:08:11 -0800

    implemented plaid and dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,6 +22,7 @@ import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
+import PlaidLink from "./PlaidLink";
 
 const AuthForm = ({ type }: { type: string }) => {
     const router = useRouter()
@@ -44,7 +45,19 @@ const AuthForm = ({ type }: { type: string }) => {
             // sign up with app write & create plaid token
 
             if (type === "sign-up") {
-                const newUser = await signUp(data);
+                const userData = {
+                    firstName: data.firstName!,
+                    lastName: data.lastName!,
+                    address1: data.address1!,
+                    city: data.city!,
+                    state: data.state!,
+                    postalCode: data.postalCode!,
+                    dateOfBirth: data.dateOfBirth!,
+                    ssn: data.ssn!,
+                    email: data.email,
+                    password: data.password
+                }
+                const newUser = await signUp(userData);
                 setUser(newUser)
             }
 
@@ -99,7 +112,12 @@ const AuthForm = ({ type }: { type: string }) => {
                 </div>
             </header>
             {user ? (
-                <div className="flex flex-col gap-4">PLAID LINK COMPONENT</div>
+                <div className="flex flex-col gap-4">
+                    <PlaidLink 
+                        user={user}
+                        variant="primary"
+                    />
+                </div>
             ) : (
                 <>
                     <Form {...form}>
